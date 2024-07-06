5d3904bcc1aaf78f29eb2167720ddc87cb4b724a
Roman 4im <smartware10@gmail.com>
2024-07-06 14:43:21 +0300

    Edit config

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -29,7 +29,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_section_option("alembic", "sqlalchemy.url", settings.db_url)
+config.set_section_option("alembic", "sqlalchemy.url", settings.db.url)
 
 
 def run_migrations_offline() -> None:
--- src/core/config.py
+++ src/core/config.py
@@ -1,15 +1,21 @@
 from pathlib import Path
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 BASE_DIR = Path(__file__).parent.parent
+DB_PATH = BASE_DIR / "market_db.sqlite3"
 
 
-class Setting(BaseSettings):
+class DbSettings(BaseModel):
     # DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    echo: bool = False
+
+
+class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/market_db.sqlite3"
-    db_echo: bool = False
+    db: DbSettings = DbSettings()
 
 
-settings = Setting()
+settings = Settings()
