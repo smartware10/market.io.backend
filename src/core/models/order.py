588dbd47cec4a1eb054cd9c7e16534b37ece6350
Roman 4im <smartware10@gmail.com>
2024-07-08 13:17:30 +0300

    Create order model table

---
+++ src/core/models/order.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from .order_product_association import order_product_association_table
+
+if TYPE_CHECKING:
+    from .product import Product
+
+
+class Order(Base):
+    promocode: Mapped[str | None]
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=datetime.utcnow,
+    )
+    products: Mapped[list["Product"]] = relationship(
+        secondary=order_product_association_table,
+        back_populates="orders",
+    )
