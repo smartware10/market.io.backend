440f651ca8e68ed21b02e49888abb51026b6b878
Roman 4im <smartware10@gmail.com>
2024-07-08 13:17:03 +0300

    Create model order product association table

---
+++ src/core/models/order_product_association.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Table, Column, ForeignKey, UniqueConstraint, Integer
+
+from .base import Base
+
+# note for a Core table, we use the sqlalchemy.Column construct,
+# not sqlalchemy.orm.mapped_column
+order_product_association_table = Table(
+    "order_product_association",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("order_id", ForeignKey("orders.id"), nullable=False),
+    Column("product_id", ForeignKey("products.id"), nullable=False),
+    UniqueConstraint("order_id", "product_id", name="idx_unique_order_product"),
+)
