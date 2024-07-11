8de678a6dca93d523fae43e982d493288a1d4a59
Roman 4im <smartware10@gmail.com>
2024-07-11 12:28:20 +0300

    Add new config for JWT auth

--- src/auth/utils_jwt.py
+++ src/auth/utils_jwt.py
@@ -1,4 +1,7 @@
+from datetime import timedelta, datetime
+
 import jwt
+import bcrypt
 
 from core.config import settings
 
@@ -7,9 +10,21 @@ def encoded_jwt(
     payload: dict,
     private_key: str = settings.auth_jwt.private_key_path.read_text(),
     algorithm: str = settings.auth_jwt.algorithm,
+    expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
 ):
+    to_encode = payload.copy()
+    now = datetime.utcnow()
+    if expire_timedelta:
+        expire_time = now + expire_timedelta
+    else:
+        expire_time = now + timedelta(minutes=expire_minutes)
+    to_encode.update(
+        exp=expire_time,
+        iat=now,
+    )
     encoded = jwt.encode(
-        payload=payload,
+        payload=to_encode,
         key=private_key,
         algorithm=algorithm,
     )
@@ -27,3 +42,19 @@ def decoded_jwt(
         algorithms=[algorithm],
     )
     return decoded
+
+
+def hash_password(password: str) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def validate_password(
+    password: str,
+    hashed_password: bytes,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
