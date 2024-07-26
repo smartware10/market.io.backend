4cd591677d278c8a1776e43a5cc09ed1e6c60338
Roman 4im <smartware10@gmail.com>
2024-07-26 18:52:18 +0300

    Add naming_convention and alembic downgrade base and alembic revision all tables

--- src/core/config.py
+++ src/core/config.py
@@ -12,6 +12,14 @@ class DatabaseConfig(BaseModel):
     pool_size: int = 50
     max_overflow: int = 15
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
 
 class RunConfig(BaseModel):
     host: str = "localhost"
