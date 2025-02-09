afb58c1614bb3cc69b07741c0ff1201b7f47cb6d
Roman 4im <smartware10@gmail.com>
2025-02-09 16:46:39 +0200

    Create User model

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -3,14 +3,17 @@ from typing import TYPE_CHECKING
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from fastapi_users.db import SQLAlchemyBaseUserTable
+
 from .base import Base
+from .mixins.id_int_pk import IdIntPkMixin
 
 if TYPE_CHECKING:
     from .post import Post
     from .profile import Profile
 
 
-class User(Base):
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
     username: Mapped[str] = mapped_column(String(24), unique=True)
 
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
